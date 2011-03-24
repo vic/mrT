366d3450f4023b0a4c9d62e42946ad4ad302793d
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2011-03-24 16:26:09 -0600

    Use a Hash for Git ignored files

--- lib/mrT/command-t/scanner.rb
+++ lib/mrT/command-t/scanner.rb
@@ -14,34 +14,14 @@ module CommandT
     @@ignored_files =
       (git_root && MrT.config[:use_git_ignore] &&
       # This should be a sorted list, so we can use a binary search.
-      `git ls-files --others --no-empty-directory --full-name`.split("\n")) ||
-      []
+      Hash[ `git ls-files --others --no-empty-directory --full-name`.split("\n").zip ]) ||
+      {}
 
     def path_excluded? path
       # Strip common prefix (@path) from path
       path = path[(@prefix_len + 1)..-1]
-      @@ignored_files.binary_search(path) || @@ignore_patterns.any? { |p| File.fnmatch? p, path }
+      @@ignored_files.key?(path) || @@ignore_patterns.any? { |p| File.fnmatch? p, path }
     end
 
-  end
-end
-
-class Array
-  def binary_search(value)
-    low, high = 0, self.size - 1
-
-    while high >= low do
-      mid = low + (high - low) / 2
-      comp = self[mid] <=> value
-
-      if comp == 0
-        return mid
-      elsif comp > 0
-        high = mid - 1
-      else
-        low = mid + 1
-      end
-    end
-    nil
   end
 end
