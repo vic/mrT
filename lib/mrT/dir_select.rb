d529bb98d11993d87062830d1e4f9568476eb3f6
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2011-03-27 11:41:48 -0600

    Directory selector (<Leader>d)
    Act on directories (change directory, remove directory, etc.).
    
    Can be Git aware.

---
+++ lib/mrT/dir_select.rb
@@ -0,0 +1,48 @@
+require 'mrT/command-t/filesys_finder'
+
+module MrT
+  class DirSelect < Selector/'d'
+    def prepared?
+      true
+    end
+
+    def matcher
+      @matcher ||= CommandT::FilesysFinder.new MrT.dir, cmd_t_options
+    end
+
+    def cmd_t_options
+      keys = [:max_depth, :max_files, :scan_dot_directories]
+      opts = Hash[keys.zip(MrT.config.values_at(*keys))]
+      # Search only for directories
+      opts[:directories] = true
+      opts[:files] = false
+      opts
+    end
+
+    def selected(ui)
+      File.expand_path(ui.selected, MrT.dir)
+    end
+
+    action "", "Custom shell command" do |ui, action|
+      cmd = ui.readline("echo " + action.target + " | xargs ", true, false)
+      Kernel.exec "echo '#{action.target}' | xargs #{cmd}"
+    end
+
+    action :cd, "Shell into directory" do |ui, action|
+      Dir.chdir action.target
+      Kernel.exec ENV['SHELL']
+    end
+
+    action :rmdir, "Delete directory" do |ui, action|
+      require 'fileutils'
+      FileUtils.rmdir action.target
+      exit 0
+    end
+
+    action :rm_rf, "Delete directory recursively" do |ui, action|
+      require 'fileutils'
+      FileUtils.rm_rf action.target
+      exit 0
+    end
+  end
+end
